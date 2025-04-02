b47bd1a22a024634c83cb1c7bfef4a8940aae6fd
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-04-02 14:31:14 +0000

    Add nasa-api

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import streamlit as st
+
+api_key = "oof0Z0XkFLSP1XUhFpnbrNoreUuHlHV3Cfq6Cdpn"
+url = "https://api.nasa.gov/planetary/apod?" \
+    f"api_key={api_key}"
+
+#Get the request data as a dictionary
+response1 = requests.get(url)
+data = response1.json
+
+#Extract the image title, url and explaination
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+#Download the image
+image_filepath = "img.png"
+response2 = requests.get(image_url)
+with open(image_filepath, 'wb') as file:
+    file.write(response2.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
